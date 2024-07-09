6ebe90179421490f63a3867abc0dd05c1c05a7a4
tankouideal <seiji9389@gmail.com>
2024-07-09 20:57:04 +0900

    Update nazo_checker_jquery.js

--- practice/nazo_checker_jquery.js
+++ practice/nazo_checker_jquery.js
@@ -11,34 +11,4 @@ function AnswerCheck(form){
       return false;
     }
 
-    $.ajax({
-      url: "./answer/"+form.name+send_text+".txt",
-      type: "GET",
-      dataType: "json",
-      timeout: 3000,
-    })
-    .done(function(resp){
-      // answers/{send_text}が存在する
-      $(form).next(".result").text('');
-      if(resp["type"] == "move"){  // 遷移
-        window.location.href = resp["value"];
-      }else if(resp["type"] == "alert"){  // アラート
-        alert(resp["value"]);
-      }else if(resp["type"] == "uftext"){  // フォーム下テキスト
-        $(form).next(".result").text(resp["value"]);
-      }else if(resp["type"] == "function"){  // 関数実行
-        tmpFn[resp["value"]](form);
-      }
-    })
-    .fail(function(){
-        // ./answer/{form.name}{send_text}.txtが存在しない
-        $(form).next(".result").text("「"+send_text+"」は不正解です。");
-    })
-    .always(function(){
-        // 送信が完了したら、送信ボタンを有効化する
-        $(form).find('button[type=submit]').prop('disabled', false);
-    });
-  } catch(error){
-    console.error(error);
-  }
 }
